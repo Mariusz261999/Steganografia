ffd137b81af1cf5d680fb7f811d08b19e5f40fcf
Mariusz Nieciecki <mariusz.nieciecki@o2.pl>
2021-05-08 17:57:54 +0200

    Wyglad aplikacji i podstawowa funkcjonalnosc

---
+++ Steganografia_app/app/src/main/java/com/nieciecki/steganografia/AuthorsActivity.kt
@@ -0,0 +1,11 @@
+package com.nieciecki.steganografia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthorsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authors)
+    }
+}
